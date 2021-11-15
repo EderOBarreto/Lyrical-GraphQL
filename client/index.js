38e3689ac18c946f134b1df6368b860d6eade1e1
eder <ederoliveirabarreto@gmail.com>
2021-11-15 15:14:13 -0300

    Add SongList

--- client/index.js
+++ client/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { Router, hashHistory, IndexRoute, Route } from "react-router";
+
+import SongList from "./components/SongList";
+import App from "./components/App";
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+        <SongList />
+      </Router>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
