599eb706ffc809db450548ab74bbee6e83d8ce34
eder <ederoliveirabarreto@gmail.com>
2021-11-16 09:15:05 -0300

    Add Lyric Create

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { Link } from "react-router";
 import fetchSongs from "../queries/fetchSongs";
+import deleteSong from "../queries/deleteSong";
+
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props
+      .mutate({ variables: { id } })
+      .then(() => this.props.data.refetch());
+  }
+
   renderSongs() {
-    return this.props.data.songs.map((song) => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          <Link to={`songs/${id}`}>{title}</Link>
+          <i className="material-icons " onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
         </li>
       );
     });
@@ -28,4 +39,4 @@ class SongList extends Component {
   }
 }
 
-export default graphql(fetchSongs)(SongList);
+export default graphql(deleteSong)(graphql(fetchSongs)(SongList));
